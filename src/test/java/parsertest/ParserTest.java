920986d79e7de0b6c9116e2d6acd78fd84f27b8d
troken11 <UO251857@uniovi.es>
2018-02-06 20:37:36 +0100

    Arreglado el testLoadExcelExito y el archivo test

--- src/test/java/parsertest/ParserTest.java
+++ src/test/java/parsertest/ParserTest.java
@@ -32,32 +32,39 @@ public class ParserTest {
 		List<XSSFCell> list1 = ex.getAllUsers().get(0);
 		List<XSSFCell> list2 = ex.getAllUsers().get(1);
 		List<XSSFCell> list3 = ex.getAllUsers().get(2);
+
 		StringBuilder st = new StringBuilder();
 
-		for (int i = 0; i < list1.size(); i++) {
+		for (int i = 0; i <= 3; i++) {
 			if (i != 3)
-				st.append(list1.get(i).toString() + " ");
+				st.append(list1.get(i).toString() + " / ");
+			else
+				st.append(list1.get(i).toString());
 		}
 
-		assertEquals(st.toString(), "Juan Torres Pardo juan@example.com C/ Federico García Lorca 2 Español 90500084Y ");
+		assertEquals(st.toString(), "Juan Torres Pardo / 22.971436; -43.182565 / juan@example.com / 87654321P");
 
 		st = new StringBuilder();
 
-		for (int i = 0; i < list2.size(); i++) {
+		for (int i = 0; i <= 3; i++) {
 			if (i != 3)
-				st.append(list2.get(i).toString() + " ");
+				st.append(list2.get(i).toString() + " / ");
+			else
+				st.append(list2.get(i).toString());
 		}
 
-		assertEquals(st.toString(), "Luis López Fernando luis@example.com C/ Real Oviedo 2 Español 19160962F ");
+		assertEquals(st.toString(), "Luis López Fernando / 32.97; -23.1 / luis@example.com / 19160962F");
 
 		st = new StringBuilder();
 
-		for (int i = 0; i < list3.size(); i++) {
+		for (int i = 0; i <= 3; i++) {
 			if (i != 3)
-				st.append(list3.get(i).toString() + " ");
+				st.append(list3.get(i).toString() + " / ");
+			else
+				st.append(list3.get(i).toString());
 		}
 
-		assertEquals(st.toString(), "Ana Torres Pardo ana@example.com Av. De la Constitución 8 Español 09940449X ");
+		assertEquals(st.toString(), "Ana Torres Pardo / 21.26; 50,26 / ana@example.com / 09940449X");
 	}
 	
 	@Test
