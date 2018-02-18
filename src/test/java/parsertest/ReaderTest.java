78c7a0059ed6409c31699a903793ea607c87c6f8
UO251461 <uo251461@uniovi.es>
2018-02-18 20:31:43 +0100

    Borrado codigo no usado de test

--- src/test/java/parsertest/ReaderTest.java
+++ src/test/java/parsertest/ReaderTest.java
@@ -1,5 +1,7 @@
 package parsertest;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
@@ -7,21 +9,18 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
-import parser.*;
+import org.junit.After;
+import org.junit.Test;
+
+import com.lowagie.text.DocumentException;
+
+import model.User;
+import parser.ReaderSingleton;
 import parser.reader.CSVReader;
 import parser.reader.ReadList;
 import parser.reader.XLSXReader;
 import persistence.UserFinder;
 import persistence.util.Jpa;
-
-import static org.junit.Assert.*;
-
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.junit.After;
-import org.junit.Test;
-
-import com.lowagie.text.DocumentException;
-import model.User;
 
 public class ReaderTest {
 
@@ -61,47 +60,18 @@ public class ReaderTest {
 	public void testLoadExcelFicheroNoEncontrado() throws FileNotFoundException, DocumentException {
 		ReadList ex = new XLSXReader();
 		ex.load("src/test/resources/fallo.xlsx");
-
-		assertEquals(ex.getLoadedList().size(), 3);
-
-		String list1 = ex.getLoadedList().get(0);
-		String list2 = ex.getLoadedList().get(1);
-		String list3 = ex.getLoadedList().get(2);
-
-		assertEquals(list1, "Juan Torres Pardo juan@example.com " + "español" + " C/ Federico García Lorca 2 Español 90500084Y ");
-
-		assertEquals(list2, "Luis López Fernando luis@example.com " + "español" + " C/ Real Oviedo 2 Español 19160962F ");
-
-		assertEquals(list3, "Ana Torres Pardo ana@example.com " + "español" + " Av. De la Constitución 8 Español 09940449X ");
 	}
 	
 	@Test (expected = FileNotFoundException.class)
 	public void testLoadCSVNoEncontrado() throws FileNotFoundException, DocumentException {
 		ReadList master = new XLSXReader();
 		master.load("src/test/resources/fallo.csv");
-		
-		assertEquals(master.getLoadedList().size(), 0);
 	}
 
 	@Test(expected = IOException.class)
 	public void testLoadExcelErrorExcel() throws IOException, DocumentException {
 		ReadList ex = new XLSXReader();
 		ex.load("src/test/resources/vacio.xlsx");
-
-		assertEquals(ex.getLoadedList().size(), 3);
-
-		String list1 = ex.getLoadedList().get(0);
-		String list2 = ex.getLoadedList().get(1);
-		String list3 = ex.getLoadedList().get(2);
-
-		assertEquals(list1,
-				"Juan Torres Pardo juan@example.com 10-oct-1985 C/ Federico García Lorca 2 Español 90500084Y ");
-
-		assertEquals(list2,
-				"Luis López Fernando luis@example.com 02-mar-1970 C/ Real Oviedo 2 Español 19160962F ");
-
-		assertEquals(list3,
-				"Ana Torres Pardo ana@example.com 01-ene-1960 Av. De la Constitución 8 Español 09940449X ");
 	}
 
 	@Test
